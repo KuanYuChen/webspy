e5e238dac1ec02e87e7aadf933b525ee8d51b4b8
kuan-yuchen <tasean833@gmail.com>
2016-05-20 22:30:19 +0800

    Listen all

--- w.py
+++ w.py
@@ -3,6 +3,6 @@ from http.server import HTTPServer,SimpleHTTPRequestHandler
 from socketserver import BaseServer
 import ssl
 
-httpd = HTTPServer(('localhost', 1443), SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='cert.pem', server_side=True)
+httpd = HTTPServer(('0.0.0.0', 1443), SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, ssl.PROTOCOL_SSLv2 ,certfile='cert.pem', server_side=True)
 httpd.serve_forever()
